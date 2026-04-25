6e1d67870d6c5a7620cbda2b6d7c7d8db739a001
Dwizz8 <144077116+Dwizz8@users.noreply.github.com>
2026-04-25 19:09:31 +1200

    Add files via upload

---
+++ Circuits/Compound.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circuits
+{
+    public class Compound : Gate
+    {
+        //Stores the list of gates inside the compound in this list
+        private List<Gate> _compoundGates = new List<Gate>();
+
+        //Creates two lists for the internally connected pins that get linked to the externally connected pins in the evaluate method.
+        //private List<Pin> _externalPins = new List<Pin>();
+        //private List<Pin> _internalPins = new List<Pin>();
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Compound(int x, int y) : base(x, y)
+        {
+            
+        }
+
+        /// <summary>
+        /// Makes the list a read property to be accessed from the form
+        /// </summary>
+        public List<Gate> CompoundGates
+        {
+            get { return _compoundGates; }
+        }
+
+        ///This was an attempt at making the pins easier to manage by giving the compound ownership of them, but I found it was easier 
+        ///to just manage them through the gates themselves.
+        //public List<Pin> ExternalPins 
+        //{ 
+        //    get { return _externalPins; } 
+        //}
+
+        //public List<Pin> InternalPins
+        //{
+        //    get { return _internalPins; }
+        //}
+
+        /// <summary>
+        /// Adds a gate to the compound gate list
+        /// </summary>
+        /// <param name="gate"></param>
+        public void AddGate(Gate gate)
+        {
+            //adds the gate to the compound gate list
+            _compoundGates.Add(gate);
+
+            //This ensures there is a top left corner of the compound and moves it properly
+            if (gate.Left < Left)
+            {
+                left = gate.Left;
+            }
+
+            if (gate.Top < Top)
+            {
+                top = gate.Top;
+            }
+        }
+
+        /// <summary>
+        /// Move's the compound as a whole
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public override void MoveTo(int x, int y)
+        {
+            //Moves the compound gates and pins the correct distance
+            foreach (Gate gate in _compoundGates)
+            {
+                int xDistance = x - left;
+                int yDistance = y - top;
+
+                gate.MoveTo(gate.Left + xDistance, gate.Top + yDistance);
+            }
+
+            foreach (Pin p in Pins)
+            {
+                int xDistance = x - left;
+                int yDistance = y - top;
+
+                p.X = p.X + xDistance;
+                p.Y = p.Y + yDistance;
+            }
+
+            //Resets the left and top values to the coordinates there at now
+            left = x;
+            top = y;
+        }
+
+        /// <summary>
+        /// Draws the compound, including the gates and the pins.
+        /// </summary>
+        /// <param name="paper"></param>
+        public override void Draw(Graphics paper)
+        {   
+            //goes through the list and essentially just temporarily makes it selected for it to be drawn in a correct color
+            foreach (Gate gate in _compoundGates)
+            {
+                if (selected)
+                {
+                    gate.Selected = true;
+                    gate.Draw(paper);
+                    gate.Selected = false;
+                }
+                else
+                {
+                    gate.Draw(paper);
+                }
+            }
+
+            //Moves the external compound pin locations to the correct spot in the compound.
+            //for (int i = 0; i < ExternalPins.Count; i++)
+            //{
+            //    ExternalPins[i].X = InternalPins[i].X;
+            //    ExternalPins[i].Y = InternalPins[i].Y;
+            //}
+
+            //Don't actually need to redraw the pins, As they are already externally associated with the compound and internally with the gates in the compound.
+            //This was more for a visual check to ensure the code was working as it should
+            //foreach (Pin p in Pins)
+            //{
+            //    p.Draw(paper);
+            //}
+        }
+
+        /// <summary>
+        /// The evaluate method to ensure that all the "circuitry" part of the circuits is working.
+        /// </summary>
+        /// <returns></returns>
+        public override bool Evaluate()
+        {
+            //I never call evaluate on the compound itself, only on the gates inside it.
+            return false;
+        }
+
+        /// <summary>
+        /// Clones the current compound gate, excluding the wires
+        /// </summary>
+        /// <returns></returns>
+        public override Gate Clone()
+        { 
+            //Creates the Cloned Gate
+            Compound CompClone = new Compound(left, top);
+
+            foreach (Gate c in _compoundGates)
+            {
+                //Adds all the gates inside the compound to the cloned compound
+                Gate Clonegate = c.Clone();
+                CompClone.AddGate(Clonegate);
+
+                //Loops through all the pins in the clone gates, and adds them to the clone compound so as the evaluate method works properly.
+                foreach (Pin p in Clonegate.Pins)
+                {
+                    CompClone.Pins.Add(p);
+                }
+            }
+
+            //Offsets the clone slightly so the user can see both compounds
+            CompClone.MoveTo(left, top);
+
+            return CompClone;
+        }
+    }
+}
---
+++ Circuits/NotGate.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+
+namespace Circuits
+{
+    public class NotGate : Gate
+    {
+
+        /// <summary>
+        /// Initialises the Gate.
+        /// </summary>
+        /// <param name="x">The x position of the gate</param>
+        /// <param name="y">The y position of the gate</param>
+        public NotGate(int x, int y ) : base(x, y)
+        {
+            //One input pin
+            pins.Add(new Pin(this, true, 20));
+
+            //One output pin
+            pins.Add(new Pin(this, false, 20));
+
+            MoveTo(x, y);
+        }
+
+        public override void MoveTo(int x, int y)
+        {
+            //Debugging message
+            Console.WriteLine("pins = " + pins.Count);
+            //Set the position of the gate to the values passed in
+            left = x;
+            top = y;
+            
+            // must move the pins too
+            pins[0].X = x - GAP;
+            pins[0].Y = y + HEIGHT / 2;
+            pins[1].X = x + WIDTH + GAP;
+            pins[1].Y = y + HEIGHT / 2;
+        }
+
+        /// <summary>
+        /// Specifically draws the OR Gate
+        /// </summary>
+        /// <param name="paper"></param>
+        public override void Draw(Graphics paper)
+        {
+            //Check if the gate has been selected
+            if (selected)
+            {
+                brush = selectedBrush;
+            }
+            else
+            {
+                brush = normalBrush;
+            }
+
+            //Draw each of the pins
+            foreach (Pin p in pins)
+                p.Draw(paper);
+
+            Point[] topTriangle = {
+                new Point(left, top),          // top-left
+                new Point(left + WIDTH, top + HEIGHT / 2),   // output tip (middle right)
+                new Point(left, top + HEIGHT / 2)            // middle-left
+            };
+
+            paper.FillPolygon(brush, topTriangle);
+
+            // Bottom triangle
+            Point[] bottomTriangle = {
+                new Point(left, top + HEIGHT / 2),           // middle-left
+                new Point(left + WIDTH, top + HEIGHT / 2),   // output tip
+                new Point(left, top + HEIGHT)  // bottom-left
+            };
+            paper.FillPolygon(brush, bottomTriangle);
+
+            int circleSize = 10;  // diameter of the NOT bubble
+            int circleRadius = circleSize / 2;
+
+            // Center of the output tip
+            int tipX = left + WIDTH;
+            int tipY = top + HEIGHT / 2;
+
+            // Draw circle centered on tip
+            paper.FillEllipse(brush, tipX - circleRadius, tipY - circleRadius, circleSize, circleSize);
+
+            //Note: You can also use the images that have been imported into the project if you wish,
+            //      using the code below.  You will need to space the pins out a bit more in the constructor.
+            //      There are provided images for the other gates and selected versions of the gates as well.
+            //paper.DrawImage(Properties.Resources.OrGate, Left, Top);
+        }
+
+        /// <summary>
+        /// Evaluates the NOT gate
+        /// </summary>
+        /// <returns></returns>
+        public override bool Evaluate()
+        {
+            //Ensures that the gate is connected to something before attempting to use recursion to evaluate it
+            if (pins[0].InputWire == null)
+                return false;
+
+            Gate gateA = pins[0].InputWire.FromPin.Owner;
+
+            return !gateA.Evaluate();
+        }
+
+        /// <summary>
+        /// Clones the NOT gate, offsetting it slightly so the user can see both gates
+        /// </summary>
+        /// <returns></returns>
+        public override Gate Clone()
+        {
+            NotGate Notg = new NotGate(left, top);
+
+            Notg.MoveTo(left + 10, top + 10);
+
+            return Notg;
+        }
+    }
+}
